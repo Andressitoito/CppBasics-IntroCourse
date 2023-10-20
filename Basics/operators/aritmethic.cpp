ec13f487b25070d9a1bab756283f76996197f310
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-10-20 20:20:02 -0300

    Operators
    Arithmetic operators

---
+++ Basics/operators/aritmethic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+ cout << "9 + 5 = " << 9 + 5 << endl;
+ cout << "9 - 5 = " << 9 - 5 << endl;
+ cout << "9 * 5 = " << 9 * 5 << endl;
+ cout << "9 / 5 = " << 9 / 5 << endl;
+ cout << "9 % 5 = " << 9 % 5 << endl;
+ cout << "'a' / 3 = " << 'a' / 3 << endl;
+ cout << "'a' % 3 = " << 'a' % 3 << endl;
+
+ return 0;
+}
