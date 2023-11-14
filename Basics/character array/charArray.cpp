e193ead64962bb8061a79e00340bba6ec37a0a0f
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2023-11-13 21:01:49 -0300

    Character Array Intro
    An introduction to character arrays, covering the basics of declaration, initialization using arrays of characters.

---
+++ Basics/character array/charArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ char name[100];
+
+ cout << "Enter your name: " << endl;
+ cin >> name;
+ cout << "Your name is: " << name << endl;
+
+ name[3] = 'X';
+ cout << "Your name is: " << name << endl;
+
+ return 0;
+}
