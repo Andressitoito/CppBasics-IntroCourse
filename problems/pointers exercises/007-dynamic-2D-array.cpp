d224db2fc768ffcff99ab2cc9850d9a59c3e5f47
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2024-01-02 19:16:26 -0300

    007-dynamic-2D-array
    Create and manipulate a dynamic 2D array.

---
+++ problems/pointers exercises/007-dynamic-2D-array.cpp
@@ -0,0 +1,21 @@
+/*  Create and manipulate a dynamic 2D array. */
+#include <iostream>
+using namespace std;
+
+int main() {
+ // Declare a double pointer 'matrix' for a dynamic 2D integer array
+ // with 3 rows and 4 columns.
+
+
+ // Use dynamic memory allocation to create the array.
+
+ // Initialize the array with values.
+
+ // Print the values of the array.
+
+ // Deallocate the dynamically allocated memory.
+
+ // Your code here
+
+ return 0;
+}
