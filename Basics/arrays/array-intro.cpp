b3fc4974890d9a218c785f4454c06b88f345b1c5
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2024-01-09 20:31:30 -0300

    Small update
    Using std::cout instead of cout with the whole importation.

--- Basics/arrays/array-intro.cpp
+++ Basics/arrays/array-intro.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-using namespace std;
+// using namespace std;
 
 int main()
 
 {
  int a[10] = {1, 2, 3, 4};
 
- for (int i = 0; i < 10; i++)
- {
-  cout << a[i] << " " << endl;
+ for (int i = 0; i < 10; i++) {
+  std::cout << a[i] << " " << std::endl;
  }
 
  return 0;
