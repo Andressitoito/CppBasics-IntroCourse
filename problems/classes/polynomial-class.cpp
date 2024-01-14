99895a9d1da77508c3af81c0343b13b623ca1b4e
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2024-01-14 05:11:24 -0300

    Polynomial Operator Overload: Plus and Minus Implemented, Multiplication Ongoing
    Successfully implemented operator overloads for addition and subtraction in the Polynomial class. Work is in progress for overloading the multiplication operator to enhance the functionality of the class.

--- problems/classes/polynomial-class.cpp
+++ problems/classes/polynomial-class.cpp
@@ -19,7 +19,7 @@ public:
  }
 
  ////////////////////////////////////
- //  Parameterized constructor
+ // Parameterized Constructor
  ////////////////////////////////////
  Polynomial(int arr[], int size) {
   capacity = size;
@@ -30,17 +30,38 @@ public:
   }
  }
 
- // Copy constructor
+ ////////////////////////////////////
+ // Deep Copy Constructor
+ ////////////////////////////////////
+ Polynomial(Polynomial const &p) {
+  this->capacity = p.capacity;
+  this->degCoef = new int[p.capacity];
 
+  for (int i = 0; i < capacity; i++) {
+   degCoef[i] = p.degCoef[i];
+  }
+ }
 
-
- // get capacity
- int getCapacity() { return this->capacity; }
-
+ ////////////////////////////////////
  // Copy assigment operator
+ ////////////////////////////////////
+ void operator=(Polynomial const &p) {
+  this->capacity = p.capacity;
+  this->degCoef = new int[p.capacity];
 
+  for (int i = 0; i < capacity; i++) {
+   degCoef[i] = p.degCoef[i];
+  }
+ }
 
- // Print function
+ ////////////////////////////////////
+ // Get Capacity
+ ////////////////////////////////////
+ int getCapacity() const { return this->capacity; }
+
+ ////////////////////////////////////
+ // Print Function
+ ////////////////////////////////////
  void print() const {
   for (int i = 0; i < this->capacity; i++) {
    if (i == 0 && this->degCoef != 0) {
@@ -50,7 +71,7 @@ public:
      if (degCoef[i] > 0) {
       cout << " + " << degCoef[i] << "x^" << i;
      } else {
-      cout << " - " << degCoef[i] << "x^" << i;
+      cout << " " << degCoef[i] << "x^" << i;
      }
     }
    }
@@ -58,16 +79,103 @@ public:
   cout << endl;
  }
 
- // Overload plus operator
+ ////////////////////////////////////
+ // Overload Plus Operator
+ ////////////////////////////////////
+ Polynomial &operator+=(Polynomial const &p) {
+  int size = max(this->capacity, p.capacity);
+
+  if (size > this->capacity) {
+   int *newDefCoef = new int[size];
+
+   for (int i = 0; i < this->capacity; i++) {
+    newDefCoef[i] = this->degCoef[i];
+   }
+
+   delete[] this->degCoef;
+
+   this->degCoef = newDefCoef;
+   this->capacity = size;
+  }
+
+  for (int i = 0; i < size; i++) {
+   degCoef[i] = this->degCoef[i] + p.degCoef[i];
+  }
+
+  return *this;
+ }
+
+ ////////////////////////////////////
+ // Overload Minus Operator
+ ////////////////////////////////////
+ Polynomial &operator-=(Polynomial &p) {
+  int size = std::max(this->capacity, p.capacity);
+
+  if (size > this->capacity) {
+   int *newDegCoef = new int[size];
+
+   for (int i = 0; i < size; i++) {
+    newDegCoef[i] = this->degCoef[i];
+   }
+
+   delete[] this->degCoef;
+
+   this->degCoef = newDegCoef;
+   this->capacity = size;
+  }
+
+  for (int i = 0; i < size; i++) {
+   degCoef[i] = this->degCoef[i] - p.degCoef[i];
+  }
+
+  return *this;
+ }
+
+ ////////////////////////////////////
+ // Overload Multiply Operator
+ ////////////////////////////////////
+ Polynomial &operator*=(Polynomial const &p) {
+  int size = std::max(this->capacity, p.capacity);
+
+  if (size < this->capacity) {
+   int *newDegCoef = new int[size];
+
+   for (int i = 0; i < size; i++) {
+    newDegCoef[i] = this->degCoef[i];
+   }
+
+   delete[] this->degCoef;
+
+   this->degCoef = newDegCoef;
+   this->capacity = size;
+  }
+
+  int *arrDegCoef = new int[size];
+
+  for (int i = 0; i < size; i++) {
+   arrDegCoef[i] = 0;
+   std::cout << "arDefCoef => " << arrDegCoef[i] << std::endl;
+  }
 
 
- // Overload minus operator
+  for (int i = 0; i < size; i++) {
+   for (int j = i + 1; j < size; j++) {
+    int prev = this->degCoef[i] + p.degCoef[j];
+
+    arrDegCoef[i] = this->degCoef[j] * p.degCoef[j];
+
+    
+   }
+  }
+ }
 
 
- // Overload multiply operator
 
 
  // setCoef function
+
+
+
 
  ~Polynomial() { delete[] degCoef; }
 };
@@ -77,11 +185,29 @@ int main() {
 
  Polynomial p1(arr, 6);
  Polynomial p2(p1);
+ // p1.print();
+ // p2.print();
+
+ Polynomial p3;
+
+ int arr2[5] = {1, 5, 13, -43, 5};
+ Polynomial p5(arr2, 5);
+
+ p3.print();
+
+ p3 += p1;
+
+ cout << "sum 1 => " << endl;
+ p3.print();
+
+ p3 -= p1;
+
+ cout << "sum2 => " << endl;
+ p3.print();
+
+ p1 += p5;
+ cout << "pass3 => " << endl;
  p1.print();
-
-
- p2.print();
-
 
  return 0;
 }
