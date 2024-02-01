b1e9247c4ca330ae48757c93f75a49a2224ff03a
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2024-02-01 20:22:49 -0300

    Completion of Polynomial Class with Learned Concepts
    Implemented the Polynomial class incorporating constant functions, copy constructors, and various types of constructors as per the concepts learned. This completes the small C++ exam.

--- problems/classes/polynomial-class.cpp
+++ problems/classes/polynomial-class.cpp
@@ -172,55 +172,72 @@ public:
   return *this;
  }
 
+ ////////////////////////////////////
+ // setCoef
+ ////////////////////////////////////
+ Polynomial &setCoef(int deg, int coef) {
+  int size = std::max(this->capacity, deg + 1);
 
+  if (size > this->capacity) {
+   int *newDefCoef = new int[size];
 
+   for (int i = 0; i < size; i++) {
+    newDefCoef[i] = 0;
+   }
 
- // setCoef function
+   for (int i = 0; i < this->capacity; i++) {
+    newDefCoef[i] = this->degCoef[i];
+   }
 
+   delete[] this->degCoef;
 
+   this->degCoef = newDefCoef;
+   this->capacity = size;
+  }
 
+  this->degCoef[deg] = coef;
+
+  return *this;
+ }
 
  ~Polynomial() { delete[] degCoef; }
 };
 
 int main() {
  int arr[6] = {3, 2, 0, 5, 0, 6};
-
+ std::cout << "p1 parameterized constructor" << std::endl;
  Polynomial p1(arr, 6);
- Polynomial p2(p1);
  p1.print();
+ std::cout << "p2 copy constructor" << std::endl;
+ Polynomial p2(p1);
  p2.print();
-
+ std::cout << "p3 default constructor" << std::endl;
  Polynomial p3;
  p3.print();
-
- int arr2[5] = {1, 5, 13, -43, 5};
- Polynomial p5(arr2, 5);
-
- p3 += p1;
-
- cout << "sum 1 => " << endl;
+ std::cout << "p4 overload plus operator" << std::endl;
+ int arr2[5] = {1, 5, 13, -4, 5};
+ Polynomial p4(arr2, 5);
+ p4 += p1;
+ cout << "sum p4 + p1" << endl;
  p3.print();
-
+ std::cout << "p3 overload minus operator" << std::endl;
  p3 -= p1;
-
- cout << "sum2 => " << endl;
+ cout << "minus p3 -1" << endl;
  p3.print();
-
- p1 += p5;
- cout << "pass3 => " << endl;
- p1.print();
-
+ std::cout << "overload multiplication operator" << std::endl;
  int arr7[2] = {2, 2};
- Polynomial p7(arr7, 2);
- Polynomial p8(p7);
- p7.print();
- p8.print();
-
- p7 *= p8;
-
- p7.print();
- std::cout << "capacity => " << p7.getCapacity() << std::endl;
+ Polynomial p5(arr7, 2);
+ Polynomial p6(p5);
+ p5.print();
+ p6.print();
+ std::cout << "(2 + 2x) * (2 + 2x)" << std::endl;
+ p5 *= p6;
+ p5.print();
+ std::cout << "new capacity p5" << p5.getCapacity() << std::endl;
+ std::cout << "set coef deg" << std::endl;
+ p5.setCoef(5, 5);
+ p5.print();
+ std::cout << "capacity => " << p5.getCapacity() << std::endl;
 
  return 0;
 }
