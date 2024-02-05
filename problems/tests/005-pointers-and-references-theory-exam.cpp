90b0e2d7e82f5b52fc34862619ac6b85d00efc0a
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2024-02-04 21:42:50 -0300

    Update exam
    An update of questions 9 to 10 was made.

--- problems/tests/005-pointers-and-references-theory-exam.cpp
+++ problems/tests/005-pointers-and-references-theory-exam.cpp
@@ -78,6 +78,56 @@ Implementation of data structures:
 
 Pointers play a fundamental role in constructing complex data structures like linked lists, trees, and graphs, which are essential for various algorithms and applications.
 
+9 - What are the advantages of using references in C++?
 
+Efficient value passing:
+
+When passing arguments to functions by reference, the compiler avoids creating copies of the original variables, leading to improved performance and memory usage, especially for large objects.
+
+Direct modification of original values:
+
+Functions can directly modify the values of variables passed by reference, enabling efficient data manipulation without the need to return modified values explicitly.
+
+Chained operations:
+
+References facilitate concise and readable code for chained operations, where the result of one operation becomes the input for the next. This is often used in mathematical expressions and object manipulations.
+
+Returning multiple values:
+
+While C++ doesn't directly support returning multiple values from a function, references can be used to simulate this behavior by returning multiple variables through reference arguments.
+
+Cleaner syntax:
+
+Compared to pointers, references offer a cleaner and less error-prone syntax, as they don't require explicit dereferencing using the * operator.
+
+No null values:
+
+References guarantee that they always point to a valid object, eliminating the potential for null pointer errors, which can enhance code safety.
+
+Enforcing const-correctness:
+
+References can be declared as const to enforce that the referenced object cannot be modified through the reference, ensuring data integrity in certain contexts.
+
+10 - Explain the concept of call-by-reference in C++.
+
+In call-by-reference, when you pass an argument to a function, you're passing the memory address of the original variable, rather than a copy of its value. This means:
+
+Direct access to the original variable: The function has direct access to the original variable's memory location, allowing it to potentially modify its value.
+
+No unnecessary copies: The compiler avoids creating copies of the variable, which can improve performance and memory efficiency, especially for large objects.
+
+Changes reflected outside: Any changes made to the variable within the function are directly reflected in the original variable outside the function.
+
+void swap(int &x, int &y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main() {
+    int a = 10, b = 20;
+    swap(a, b);  // Pass a and b by reference
+    // Now a = 20 and b = 10
+}
 */
 
