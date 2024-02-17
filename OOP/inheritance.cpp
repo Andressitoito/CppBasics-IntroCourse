9a595b32402e228277fb51ffaa7795331f0ad95c
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2024-02-16 23:35:39 -0300

    Implementing Parameterized Class Constructors in Inheritance
    Currently working on inheritance, specifically focusing on implementing parameterized class constructors within inheritance structures.

--- OOP/inheritance.cpp
+++ OOP/inheritance.cpp
@@ -10,6 +10,17 @@ protected:
 
 public:
  string color;
+
+ Vehicle(int z) {
+  std::cout << "Vehicle parameterized constructor" << std::endl;
+  maxSpeed = z;
+ }
+
+ // Vehicle() {
+ // std::cout << "Vehicle default constructor" << std::endl;
+ // }
+
+ ~Vehicle() { std::cout << "Vehicle destructor" << std::endl; }
 };
 
 class Car : public Vehicle {
@@ -22,20 +33,40 @@ public:
   std::cout << "numGears => " << numGears << std::endl;
   // std::cout << "Max speed : " << maxSpeed << std::endl;
  }
+
+ // Car() : Vehicle(5) {
+ //  std::cout << "default constructor parameterized" << std::endl;
+ // }
+
+ Car(int x, int y) : Vehicle(x) {
+  numGears = y;
+  std::cout << "Car parameterized constructor" << std::endl;
+ }
+
+ ~Car() { std::cout << "Car destructor" << std::endl; }
 };
 
+class HondaCity : public Car {
+public:
+ HondaCity(int x, int y) : Car(x, y) {
+  std::cout << "Honda City Default Constructor" << std::endl;
+ }
+
+ ~HondaCity() { std::cout << "Honda City Destructor" << std::endl; }
+};
 
 int main() {
- Vehicle v;
- v.color = "Blue";
+ // Vehicle v;
+ // v.color = "Blue";
  // v.maxSpeed = 4;
  // v.numTyres = 4;
 
- Car c;
+ // Car c(5);
 
-c.color = "Black";
-c.numGears = 5;
-// c.numTyres = 4;
+ HondaCity h(4, 5);
+ // c.color = "Black";
+ // c.numGears = 5;
+ // c.numTyres = 4;
 
 
  return 0;
