3be65e77ce28ff6a74e1e004e1ceb0561746dc4e
Andressitoito <60946504+Andressitoito@users.noreply.github.com>
2024-03-09 17:13:38 -0300

    Polymorphism Overview and Examples
    Outlined the concept of polymorphism and provided a brief overview, including examples of both compile-time and run-time polymorphism.

---
+++ OOP/polymorphism.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+public:
+ string color;
+
+ void print() { std::cout << "Vehicle" << std::endl; }
+};
+
+class Car : public Vehicle {
+public:
+ int numGears;
+ void print() { std::cout << "Car" << std::endl; }
+};
+
+
+int main() {
+ Vehicle v;
+
+ Car c;
+
+ v.print();
+ c.print();
+
+ Vehicle *v1 = new Vehicle;
+ Vehicle *v2;
+ // v2 = &v1;
+ v2 = &c;
+
+ v2->print();  // We can only access base class properties
+
+ return 0;
+}
